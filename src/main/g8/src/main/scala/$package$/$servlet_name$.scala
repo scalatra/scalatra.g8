b14c7edaab6ad70a84021f4c4e5d85bb476f1aa0
Jared Armstrong <jrod@jaredarmstrong.name>
2012-02-02 20:20:41 -0500

    fix

---
+++ src/main/g8/src/main/scala/$package$/$servlet_name$.scala
@@ -0,0 +1,24 @@
+package $package$
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class $servlet_name$ extends ScalatraServlet with ScalateSupport {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+  notFound {
+    // Try to render a ScalateTemplate if no route matched
+    findTemplate(requestPath) map { path =>
+      contentType = "text/html"
+      layoutTemplate(path)
+    } orElse serveStaticResource() getOrElse resourceNotFound() 
+  }
+}
