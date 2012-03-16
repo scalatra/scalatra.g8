26b8bf1a54288349d8f5d4266a0c85f4238ead92
Ross A. Baker <baker@alumni.indiana.edu>
2012-03-16 00:04:40 -0400

    Revert "Use ScalatraListener to bootstrap the app."  Should be on develop.
    This reverts commit cfa1f0d2d026f64857524187b32d41e77b3610bf.

--- src/main/g8/src/main/scala/Scalatra.scala
+++
@@ -1,8 +0,0 @@
-import $package$._
-import org.scalatra._
-
-class Scalatra extends LifeCycle {
-  override def init(context: ApplicationContext) {
-    context.mount(new $servlet_name$, "/*")
-  }
-}
