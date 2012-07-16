e89d1b37b4671882ffade8ee4d5d4ac9818421a5
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-07-16 09:02:37 +0200

    Replace ApplicationContext with ServletContext in the listener

--- src/main/g8/src/main/scala/Scalatra.scala
+++ src/main/g8/src/main/scala/Scalatra.scala
@@ -1,8 +1,9 @@
 import $package$._
 import org.scalatra._
+import javax.servlet.ServletContext
 
 class Scalatra extends LifeCycle {
-  override def init(context: ApplicationContext) {
+  override def init(context: ServletContext) {
     context.mount(new $servlet_name$, "/*")
   }
 }
