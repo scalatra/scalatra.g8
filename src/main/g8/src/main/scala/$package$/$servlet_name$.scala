e58e02d72a3cb26d088f51f90bbaf958ee7d933e
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-08-10 14:13:12 +0200

    Reset content type to null in notFound handler

--- src/main/g8/src/main/scala/$package$/$servlet_name$.scala
+++ src/main/g8/src/main/scala/$package$/$servlet_name$.scala
@@ -15,6 +15,8 @@ class $servlet_name$ extends ScalatraServlet with ScalateSupport {
   }
 
   notFound {
+    // remove content type in case it was set through an action
+    contentType = null 
     // Try to render a ScalateTemplate if no route matched
     findTemplate(requestPath) map { path =>
       contentType = "text/html"
