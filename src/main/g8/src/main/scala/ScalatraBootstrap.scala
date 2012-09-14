092f7562f90bf89f6070a7679a3adb26f83e61fb
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-09-14 15:03:31 -0700

    Rename bootstrap file

--- src/main/g8/src/main/scala/ScalatraBootstrap.scala
+++ src/main/g8/src/main/scala/ScalatraBootstrap.scala
@@ -2,7 +2,7 @@ import $package$._
 import org.scalatra._
 import javax.servlet.ServletContext
 
-class Bootstrap extends LifeCycle {
+class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     context.mount(new $servlet_name$, "/*")
   }
