5d9dec19b559ccab00a7ef4ab2fd7750648902b2
Ivan Porto Carrero <ivan@flanders.co.nz>
2013-02-07 11:25:28 +0100

    Add missing imports

--- src/main/g8/src/main/scala/$package$/$name__Camel$Stack.scala
+++ src/main/g8/src/main/scala/$package$/$name__Camel$Stack.scala
@@ -2,6 +2,10 @@ package $package$
 
 import org.scalatra._
 import scalate.ScalateSupport
+import org.fusesource.scalate.{ TemplateEngine, Binding }
+import org.fusesource.scalate.layout.DefaultLayoutStrategy
+import javax.servlet.http.HttpServletRequest
+import collection.mutable
 
 trait $name;format="Camel"$Stack extends ScalatraServlet with ScalateSupport {
 
