03d730c5020a3bfd9ba82ecb01a3bfbf47d61a03
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-07-20 20:40:44 +0100

    Updating servlet version in project/build.scala

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -26,7 +26,7 @@ object $name;format="Camel"$Build extends Build {
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.1.3.v20140225" % "container",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
