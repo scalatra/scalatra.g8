518392bc0c9a5f0332b36a8f08ef30d61c798c1b
Ivan Porto Carrero <ivan@flanders.co.nz>
2014-09-15 09:35:53 -0700

    Update dependencies for scalatra 2.3

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -25,10 +25,10 @@ object $name;format="Camel"$Build extends Build {
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "9.1.3.v20140225" % "container",
-        "org.eclipse.jetty" % "jetty-plus" % "9.1.3.v20140225" % "container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.1.5.v20140505" % "container",
+        "org.eclipse.jetty" % "jetty-plus" % "9.1.5.v20140505" % "container",
+        "javax.servlet" % "javax.servlet-api" % "3.1.0"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
