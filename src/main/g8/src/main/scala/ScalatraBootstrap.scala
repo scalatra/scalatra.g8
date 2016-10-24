30541b01fc2bdf7bb9f63c8017a2e1e15ec2caaa
Rocky Fan <jufan@expedia.com>
2016-10-24 10:34:35 -0700

    add scala metrics in bootstrap

--- src/main/g8/src/main/scala/ScalatraBootstrap.scala
+++ src/main/g8/src/main/scala/ScalatraBootstrap.scala
@@ -2,8 +2,22 @@ import $package$._
 import org.scalatra._
 import javax.servlet.ServletContext
 
-class ScalatraBootstrap extends LifeCycle {
+import org.scalatra.metrics.MetricsBootstrap
+implicit import org.scalatra.metrics.MetricsSupportExtensions._
+
+class ScalatraBootstrap extends LifeCycle with MetricsBootstrap {
+
+  //override val metricRegistry = ???
+  //override val healthCheckRegistry = ???
+
   override def init(context: ServletContext) {
+
+    context.mountMetricsAdminServlet("/metrics-admin")
+    context.mountHealthCheckServlet("/health")
+    context.mountMetricsServlet("/metrics")
+    context.mountThreadDumpServlet("/thread-dump")
+    context.installInstrumentedFilter("/test/*")
+
     context.mount(new $servlet_name$, "/*")
   }
 }
