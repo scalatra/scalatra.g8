67b56cfaeb46defd88ea92cb45de5a412bc8152f
Magnolia K <magnolia.k@icloud.com>
2018-04-19 06:40:39 +0900

    applied the sbt-scalariform plugin

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,4 @@
 addSbtPlugin("org.foundweekends.giter8" % "sbt-giter8" % "0.11.0-M3")
+addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.8.2")
 
 scalacOptions += "-deprecation"
--- src/main/g8/src/test/scala/$package$/$servlet_name$Tests.scala
+++ src/main/g8/src/test/scala/$package$/$servlet_name$Tests.scala
@@ -6,8 +6,8 @@ class $servlet_name$Tests extends ScalatraFunSuite {
   
   addServlet(classOf[$servlet_name$], "/*")
     
-  test("GET / on $servlet_name$ should return status 200"){
-    get("/"){
+  test("GET / on $servlet_name$ should return status 200") {
+    get("/") {
       status should equal (200)
     }
   }
